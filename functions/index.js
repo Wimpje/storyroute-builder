208366196cedd7b98cab22057d4340a191ee871a
Wim Horinga <wim.horinga@exlibrisgroup.com>
2020-02-26 15:38:49 +0100

    Updated cloud functions & security

--- functions/index.js
+++ functions/index.js
@@ -6,19 +6,37 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+const emails = new Set(['wimhoringa@gmail.com', 'pkhoringa@gmail.com']);
+const admin = require('firebase-admin');
 
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: 'https://wims-test-2c2e4.firebaseio.com'
+});
+
+exports.setCustomClaims = functions.auth.user().onCreate(user => {
+  let promise = Promise.resolve();
+  if (emails.has(user.email)) {
+    promise = admin
+      .auth()
+      .setCustomUserClaims(user.uid, { admin: true })
+      .then(() => true);
+  }
+
+  return promise;
+});
 
 exports.Google_T2S = functions.firestore.document('pois/{ID}').onUpdate((change, context) => {
-  
+
   // if shouldn't convert, don't convert...
-  if(!change.after.data().description.convertToVoice) 
+  if (!change.after.data().description.convertToVoice)
     return;
 
 
   const newData = change.after.data().description != null && change.before.data().description == null
   const changedData = change.after.data().description != change.before.data().description
-
-  if (newData || changedData) {
+  const convertToVoiceSet = change.after.data().convertToVoice
+  if (newData || changedData || convertToVoiceSet) {
     // TODO, check for description_nl or description_en type fields, and convert accordingly
 
     async function textToSpeechRequest() {
@@ -32,22 +50,22 @@ exports.Google_T2S = functions.firestore.document('pois/{ID}').onUpdate((change,
         const textToSpeech = require('@google-cloud/text-to-speech'); // Imports the Google Cloud client library
         const client = new textToSpeech.TextToSpeechClient(); // Creates a client
 
-        let fileName = change.after.data().title.replace(/ /g,"_"); // replace spaces with underscores in the file name
+        let fileName = change.after.data().title.replace(/ /g, "_"); // replace spaces with underscores in the file name
         fileName = myWordFile.toLowerCase(); // convert the file name to lower case
         fileName = myWordFile + audioFormat; // append .mp3 to the file name;
 
         // copied from https://cloud.google.com/blog/products/gcp/use-google-cloud-client-libraries-to-store-files-save-entities-and-log-data
-        const {Storage} = require('@google-cloud/storage');
+        const { Storage } = require('@google-cloud/storage');
         const storage = new Storage();
         const bucket = storage.bucket('voice');
         var file = bucket.file(language + '/' + fileName);
 
         const request = { // Construct the request
-          input: {text},
+          input: { text },
           // Select the language and SSML Voice Gender (optional)
-          voice: {languageCode: language, ssmlGender: 'FEMALE'},
+          voice: { languageCode: language, ssmlGender: 'FEMALE' },
           // Select the type of audio encoding
-          audioConfig: {audioEncoding: 'MP3'},
+          audioConfig: { audioEncoding: 'MP3' },
         };
 
         const options = { // construct the file to write
@@ -64,17 +82,18 @@ exports.Google_T2S = functions.firestore.document('pois/{ID}').onUpdate((change,
         // Write the binary audio content to a local file
         // response.audioContent is the downloaded file
         return await file.save(response.audioContent, options)
-        .then(function() {
-          console.log("File written to Firebase Storage.")
-        })
-        .catch(function(error) {
-          console.error(error);
-        });
-    } // close try
-    catch (error) {
-      console.error(error);
-    } // close catch
-  } // close async
-  textToSpeechRequest();
-} // close if
+          .then(function () {
+            change.after.update({ voiceFile: file })
+            console.log("File written to Firebase Storage.")
+          })
+          .catch(function (error) {
+            console.error(error);
+          });
+      } // close try
+      catch (error) {
+        console.error(error);
+      } // close catch
+    } // close async
+    textToSpeechRequest();
+  } // close if
 }); // close Google_T2S
